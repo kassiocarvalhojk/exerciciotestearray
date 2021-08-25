9f0b22b76c010714cf7f3ec4440dd50af88dd5f1
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-08-25 20:46:15 -0300

    Update Arranjo.java

--- Arranjo.java
+++ Arranjo.java
@@ -6,20 +6,20 @@ package arranjoExercicio;
 /**
  * @author kassiocarvalho
  *
+ *
  */
 public class Arranjo {
 
 	public Integer menor() {
 		// TODO Auto-generated method stub
 		int[] array = {940, 880, 830, 790, 750, 660, 650, 590, 510, 440};
+		int menor = 0;
 		
-		int total = 0;
+		for (int i = 0; i < array.length; i++)
+			if (menor<array[i])
+				menor = array[i];
+			return menor;
 		
-		for (int i : array);
-			total += i;
-			
-		System.out.println("Total de elementos: %d\n", total);
-		return null;
 	}
 
 }
