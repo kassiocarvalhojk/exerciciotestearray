f110c046fea88041d14926f796e9f35cfbbce4e3
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-08-25 20:20:08 -0300

    Add files via upload

---
+++ Arranjo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package arranjoExercicio;
+
+/**
+ * @author kassiocarvalho
+ *
+ */
+public class Arranjo {
+
+	public Integer menor() {
+		// TODO Auto-generated method stub
+		int[] array = {940, 880, 830, 790, 750, 660, 650, 590, 510, 440};
+		
+		int total = 0;
+		
+		for (int i : array);
+			total += i;
+			
+		System.out.println("Total de elementos: %d\n", total);
+		return null;
+	}
+
+}
---
+++ TesteCaseArranjo.java
@@ -0,0 +1,14 @@
+package arranjoExercicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TesteCaseArranjo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
