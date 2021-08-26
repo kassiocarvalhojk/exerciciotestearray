990f7324a2a4e4415d21873f4abedd9a896f3158
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-08-25 21:51:04 -0300

    Update Arranjo.java

--- Arranjo.java
+++ Arranjo.java
@@ -12,12 +12,12 @@ public class Arranjo {
 
 	public Integer menor() {
 		// TODO Auto-generated method stub
-		Integer[] a = {940, 880, 830, 790, 750, 660, 650, 590, 510, 440};
+		Integer[] array = {940, 880, 830, 790, 750, 660, 650, 590, 510, 440};
 		int menor = 0;
 		
-		for (int i = 0; i < a.length; i++)
-			if (menor<a[i])
-				menor = a[i];
+		for (int i = 0; i < array.length; i++)
+			if (menor<array[i])
+				menor = array[i];
 			return menor;
 		
 	}
