8a4612d58efebf20675886e54951b60cef61f6e6
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-08-25 21:50:41 -0300

    Update TesteCaseArranjo.java

--- TesteCaseArranjo.java
+++ TesteCaseArranjo.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TesteCaseArranjo {
 
 	@Test
-	void menoTest() {
+	void menorTest() {
 		
 		Arranjo a = new Arranjo();
 		
@@ -17,23 +17,23 @@ class TesteCaseArranjo {
 		
 		// Testa se o maior é 940
 		
-		//assertEquals(940, a.maior(), "O maior deve ser 940");
+		assertEquals(940, a.maior(), "O maior deve ser 940");
 		
 		// Testa se a soma é 7540
 		
-		//assertEquals(7540, a.soma(), "A soma é 940");
+		assertEquals(7540, a.soma(), "A soma é 940");
 		
 		// Testa a quantidade de vezes que o nuúmero  3 aparece
 				
-		//assertEquals(0, a.repeticoes(3), "940 aparece 2 vezes");
+		assertEquals(0, a.repeticoes(3), "940 aparece 2 vezes");
 		
 		// Testa a quantidade de vezes que o nuúmero  3 aparece
 		
-		//assertEquals(1, a.repeticoes(790), "940 aparece 2 vezes");
+		assertEquals(1, a.repeticoes(790), "940 aparece 2 vezes");
 		
 		// Testa a quantidade de vezes que o nuúmero  3 aparece
 		
-		//assertEquals(2, a.repeticoes(940), "940 aparece 2 vezes");
+		assertEquals(2, a.repeticoes(940), "940 aparece 2 vezes");
 						
 				
 				
