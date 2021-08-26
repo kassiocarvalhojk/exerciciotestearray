55d449feedcdd167f5748d63f8441f3bccfb4c25
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-08-25 21:39:22 -0300

    Add files via upload

---
+++ TesteCaseArranjo.java
@@ -0,0 +1,43 @@
+package arranjoExercicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TesteCaseArranjo {
+
+	@Test
+	void menoTest() {
+		
+		Arranjo a = new Arranjo();
+		
+		// Testa se o menor é 510
+		
+		assertEquals(510, a.menor(), "O menor deve ser 440");
+		
+		// Testa se o maior é 940
+		
+		//assertEquals(940, a.maior(), "O maior deve ser 940");
+		
+		// Testa se a soma é 7540
+		
+		//assertEquals(7540, a.soma(), "A soma é 940");
+		
+		// Testa a quantidade de vezes que o nuúmero  3 aparece
+				
+		//assertEquals(0, a.repeticoes(3), "940 aparece 2 vezes");
+		
+		// Testa a quantidade de vezes que o nuúmero  3 aparece
+		
+		//assertEquals(1, a.repeticoes(790), "940 aparece 2 vezes");
+		
+		// Testa a quantidade de vezes que o nuúmero  3 aparece
+		
+		//assertEquals(2, a.repeticoes(940), "940 aparece 2 vezes");
+						
+				
+				
+				
+	}
+
+}
